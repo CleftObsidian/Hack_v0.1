64347c4250012b3e8751d5d380174a296bf2b145
CleftObsidian <walkpark@khu.ac.kr>
2021-09-01 08:30:56 +0900

    [ADD] added raw game project with Unity Engine

---
+++ Hack_v0.1/Assets/Scripts/Activator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    public GameObject line;
+    public KeyCode key; // 입력 버튼
+    public GameObject n; // createMode에서 생성될 노트
+
+    private bool isActive = false; // 판정선에 닿았는지 체크
+    private GameObject note; // 노트
+
+	void Update () {
+        if (GameManager.instance.createMode)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                Instantiate(n, transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(key))
+            {
+                line.SetActive(true);
+            }
+
+            if (Input.GetKeyUp(key))
+            {
+                line.SetActive(false);
+            }
+
+            if (Input.GetKeyDown(key) && isActive)
+            {
+                Destroy(note);
+                GameManager.instance.AddScore();
+                GameManager.instance.AddCombo();
+                isActive = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isActive = true;
+
+        if (other.gameObject.tag == "Note")
+        {
+            note = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        isActive = false;
+    }
+}
---
+++ Hack_v0.1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public Text scoreText;
+    public Text comboText;
+    public Text multiScoreText;
+    public bool createMode;
+
+    private int score = 0;
+    private int combo = 0;
+    private int multiScore;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Start () {
+        score = 0;
+        scoreText.text = "" + score;
+
+        combo = 0;
+        comboText.text = combo + " Combo";
+
+        multiScore = 1;
+        multiScoreText.text = "";
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            ResetCombo();
+    }
+
+    public void AddScore()
+    {
+        score += 100 * multiScore;
+
+        UIUpdate();
+    }
+
+    public void AddCombo()
+    {
+        combo++;
+
+        if (combo >= 40)
+        {
+            multiScore = 5;
+        }
+        else if (combo >= 30)
+        {
+            multiScore = 4;
+        }
+        else if (combo >= 20)
+        {
+            multiScore = 3;
+        }
+        else if (combo >= 10)
+        {
+            multiScore = 2;
+        }
+        else
+        {
+            multiScore = 1;
+        }
+
+        UIUpdate();
+    }
+
+    public void ResetCombo()
+    {
+        combo = 0;
+
+        UIUpdate();
+    }
+
+    public void UIUpdate()
+    {
+        scoreText.text = "" + score;
+        comboText.text = combo + " Combo";
+
+        if (multiScore > 1)
+        {
+            multiScoreText.text = "Score X" + multiScore;
+        }
+        else
+        {
+            multiScoreText.text = "";
+        }
+    }
+}
